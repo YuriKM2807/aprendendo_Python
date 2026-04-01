86f50dcc09951ca0b4c077a677603f746cb0ff31
YuriKM2807 <yurikreuzbergmarcelino@gmail.com>
2026-04-01 11:52:18 -0300

    Criando mercado.py

---
+++ mecadinho.py
@@ -0,0 +1,32 @@
+import datetime 
+class market:
+    def __init__(self):
+        print("Mercado de Prompt")
+        print("----------------------")
+        print("1 -- Realizar compra")
+        print("2 -- Verificar Preco")
+        print("1 -- Desligar")
+        print("----------------------")
+    def cadastrarProduto():
+        print("Cadastro de Produto")
+        print("----------------------")
+        print("1. Re-inventario de Produto")
+        print("2. Novo Produto")
+        print("3. Editar Produto")
+        print("4. Verificar Produto")
+        print("----------------------")
+
+class produto:
+    def __init__(self):
+        id = int 
+        preco = int
+        nome = str
+        marca = str
+        dataFabrica = datetime.date  
+        dataValidade= datetime.date
+
+
+        
+        
+max = market()
+max 
