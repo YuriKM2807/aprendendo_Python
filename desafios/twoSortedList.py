cdcdb2849c74960e1a53822c1391cbba59ed550e
YuriKM2807 <yurikreuzbergmarcelino@gmail.com>
2026-05-19 09:30:20 -0300

    TwoSortedList finalizado

--- desafios/twoSortedList.py
+++ desafios/twoSortedList.py
@@ -1,14 +1,21 @@
-listA = [1, 2, 4]
-listB = [1, 3, 4]
-listFinal = []
-a = 0
-b = 0
-while a < len(listA) or b < len(listB):
-    while a < b:
-        listFinal.append(listA[a])
-        a += 1
-    while b < a:
-        listFinal.append(listB[b])
-        b += 1
-        
-print(listFinal)
+def twoSortedList(listA, listB):
+    
+    listFinal = []
+    a = 0
+    b = 0
+    while a < len(listA) and b < len(listB):
+        if listA[a] < listB[b]:
+            listFinal.append(listA[a])
+            a += 1
+        else:
+            listFinal.append(listB[b])
+            b += 1
+
+    listFinal += listA[a:]
+    listFinal += listB[b:]
+    print(listFinal)
+
+
+list1 = [1, 1, 2, 4, 7, 10]
+list2 = [1, 3, 4, 5]
+twoSortedList(list1, list2)
